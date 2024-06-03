5daf7ca9115cce4b87b591cf29bfd9173618bd15
xXFrostXx <48761453+xXFrostXx1@users.noreply.github.com>
2024-06-03 07:30:59 +0200

    add new variables

--- commands/pay.js
+++ commands/pay.js
@@ -2,30 +2,30 @@ require('dotenv').config();
 const axios = require('axios');
 const getUserInput = require('prompt-sync')({ sigint: true });
 
-async function processPayment() { 
+async function initiatePaymentProcess() { 
   try {
     console.log("Available payment methods: 1. Credit Card, 2. PayPal");
-    const selectedPaymentMethod = getUserInput("Please choose your payment method (Enter 1 for Credit Card or 2 for PayPal): "); 
-    const paymentAmount = getUserInput("Enter the amount you wish to pay: "); 
+    const chosenPaymentMethodInput = getUserInput("Please choose your payment method (Enter 1 for Credit Card or 2 for PayPal): "); 
+    const paymentAmountInput = getUserInput("Enter the amount you wish to pay: "); 
 
-    if (isNaN(paymentAmount) || paymentAmount <= 0) throw new Error("Invalid amount");
-    if (selectedPaymentMethod !== '1' && selectedPaymentMethod !== '2') throw new Error("Invalid payment method"); 
+    if (isNaN(paymentAmountInput) || paymentAmountInput <= 0) throw new Error("Invalid amount");
+    if (chosenPaymentMethodInput !== '1' && chosenPaymentMethodInput !== '2') throw new Error("Invalid payment method"); 
     
-    const paymentDetails = { 
-      method: selectedPaymentMethod === '1' ? 'Credit Card' : 'PayPal',
-      amount: parseFloat(paymentAmount),
+    const paymentInformation = { 
+      method: chosenPaymentMethodInput === '1' ? 'Credit Card' : 'PayPal',
+      amount: parseFloat(paymentAmountInput),
     };
 
-    const paymentResponse = await axios.post(`${process.env.BACKEND_API_URL}/create-payment`, paymentDetails); 
+    const paymentSubmissionResponse = await axios.post(`${process.env.BACKEND_API_URL}/create-payment`, paymentInformation); 
 
-    if (paymentResponse.data && paymentResponse.data.success) {
-      console.log(`Payment processed successfully. Please follow these instructions: ${paymentResponse.data.instructions}`); 
+    if (paymentSubmissionResponse.data && paymentSubmissionResponse.data.success) {
+      console.log(`Payment processed successfully. Please follow these instructions: ${paymentSubmissionResponse.data.instructions}`); 
     } else {
       throw new Error("Failed to process payment");
     }
-  } catch (error) {
-    console.error(`Error processing payment: ${error.message}`); 
+  } catch (paymentError) {
+    console.error(`Error processing payment: ${paymentError.message}`); 
   }
 }
 
-processPayment(); 
+initiatePaymentProcess();
